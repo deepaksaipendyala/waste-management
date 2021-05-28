b403300b6348b11469b077e62e2456ef82dab699
Deepak sai Pendyala <>
2021-05-28 20:19:09 +0000

    avg function

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
-int avg();
+float avg(float degrad[],float plastic[],float elec[],int area,int h,int d,int f,float finall);
 int reward();
-int earn();
+float earn(float degrad[],float plastic[],float elec[],int area,int h,int d,int f,float finall);
 int percentage();
 int customer();
 
 int main() {
  int option,choice,h,d,area,x,j,k;
+ float finall=0;
+ float result=0;
  printf("Namaskar..! Welcome to Waste Management Calculator\nif you are consumer, please type 1\nif you are from Municipal Authority, please press 2:");
  scanf("%d", &option);
 if (option==2){
@@ -19,8 +21,10 @@ if (option==2){
   scanf("%d", &d);
   x=h;
   k=0;
-  float degrad[d*h],plastic[d*h],elec[d*h];
+  printf("If you want to computes the statistics of the waste collection per area, Please enter 1\n If you want to reward the house which gives less quantity of waste, please enter 2\nIf you want to Compute the total earning for the municipality, please enter 3\nIf you want to know what percentage of the waste is from what category, please enter 4:");
+scanf("%d",&choice);
   for (int f=0;f<area;f++){
+    float degrad[d*h],plastic[d*h],elec[d*h];
     printf("ENTER THE DETAILS IN AREA %d\n",f+1);
     for (int g=0;g<d;g=g+3){
     printf("enter the quantity of degradable waste in kg on day %d\n",g+1);
@@ -54,26 +58,25 @@ h=h+h;
      printf("Enter quantity in house %d:",j+1);
      scanf("%f",&elec[i]);
     j=j+1;             }
-      k=k+h;
-      h=h+h;
   }
-  }
-  printf("If you want to computes the statistics of the waste collection per area, Please enter 1\n If you want to reward the house which gives less quantity of waste, please enter 2\nIf you want to Compute the total earning for the municipality, please enter 3\nIf you want to know what percentage of the waste is from what category, please enter 4:");
-scanf("%d",&choice);
 if (choice==1){
-  avg(degrad,plastic,elec,area,h,d);
+   result=avg(degrad,plastic,elec,area,x,d,f,finall);
+   finall=finall+result;
 }else if (choice==2){
-  reward(degrad,plastic,elec,area,h,d);
+  reward(degrad,plastic,elec,area,x,d);
 }else if (choice==3){
-  earn(degrad,plastic,elec,area,h,d);
+  result=earn(degrad,plastic,elec,area,x,d,f,finall);
+  finall=finall+result;
 }else if (choice==4){
-  percentage(degrad,plastic,elec,area,h,d);
+  percentage(degrad,plastic,elec,area,x,d);
 }else printf("wrong input, sorry! run again");
-
+ printf("\n\n\n"); }
 }
 else if (option==1){
   customer();
 }else printf("wrong input, sorry! run again");
+
+
 
 /*charges : 1kg degredable waste - 5rs
 1kg plastic waste - 10rs
@@ -86,10 +89,53 @@ return 0;
 
 
 
-int avg(float degrad[],float plastic[],float elec[],int area,int h,int d){
+float avg(float degrad[],float plastic[],float elec[],int area,int h,int d,int f,float finall){
 
+float tdeg,tplas,telec,total;
+int a,b,c,g,x,k,j;
+float avg=0;
+tdeg=0;
+x=h;
+k=0;
 
-return 0;
+    for (int g=0;g<d;g=g+3){
+  for (int i=k;i<h;i++){
+    a=0;
+    a=degrad[i];
+    tdeg=tdeg+a;
+}
+k=k+h;
+h=h+h;
+ }
+    tplas=0;
+    k=0;
+    h=x;
+    for(int g=1;g<d;g=g+3){
+    for (int i=k;i<h;i++){
+     b=0;
+     b=plastic[i];
+     tplas=tplas+b;    }
+      k=k+h;
+      h=h+h;
+                      }
+  telec=0;
+  k=0;
+  h=x;
+  for (int g=2;g<d;g=g+3){
+   for(int i=k;i<h;i++){
+     c=0;
+     c=elec[i];
+    telec=telec+c; }
+      k=k+h;
+      h=h+h;
+  }
+  total=tdeg+tplas+telec;
+  avg=total/3;
+ finall+=avg;
+if(area==f+1){
+printf("So Finally, Average waste in all Categories of waste in Municipality for %d days in %d Area/Areas is %.2f kgs",x,f,finall);
+}
+return finall;
 }
 
 int reward(float degrad[],float plastic[],float elec[],int area,int h,int d){
@@ -100,17 +146,14 @@ return 0;
 }
 
 
-int earn(float degrad[],float plastic[],float elec[],int area,int h,int d){
+float earn(float degrad[],float plastic[],float elec[],int area,int h,int d,int f,float finall){
 
-
-float tdeg,tplas,telec,total,finall;
-int a,b,c,g,f,x,k,j;
+float tdeg,tplas,telec,total;
+int a,b,c,g,x,k,j;
 tdeg=0;
 x=h;
 k=0;
 
-
-for (int f=0;f<area;f++){
     for (int g=0;g<d;g=g+3){
   for (int i=k;i<h;i++){
     a=0;
@@ -146,13 +189,11 @@ h=h+h;
       h=h+h;
   }
   total=tdeg+tplas+telec;
-  finall+=total;
-  k=0;
-  h=x;
-    }
-printf("So Finally, Total Earnings of the Municipality for %d days in %d Area/Areas is %.2f rupees",x,area,finall);
-
-return 0;
+ finall+=total;
+if(area==f+1){
+printf("So Finally, Total Earnings of the Municipality for %d days in %d Area/Areas is %.2f rupees",x,f,finall);
+}
+return finall;
 }
 
 int percentage(float degrad[],float plastic[],float elec[],int area,int h,int d){
